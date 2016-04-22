22e42d7d0fdf5454bf300784c7f2f31cad17e417
Brian Ketelsen <bketelsen@gmail.com>
2016-04-22 18:44:37 -0400

    fix neovim install

--- post-install/2-neovim.sh
+++ post-install/2-neovim.sh
@@ -4,7 +4,7 @@ set -e
 DEBIAN_FRONTEND=noninteractive sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:neovim-ppa/unstable 
 DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get -y install python3-dev python3-pip python-dev python-pip neovim 
-sudo pip2 install neovim
-sudo pip2 install neovim
+pip2 install neovim
+pip2 install neovim
 
 
