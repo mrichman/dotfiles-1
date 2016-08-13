abc5fc0ed15011bea0b4cd3798a35b39324613b7
Brian Ketelsen <bketelsen@gmail.com>
2016-08-13 11:11:29 -0400

    odd

--- post-install/1-packages.sh
+++ post-install/1-packages.sh
@@ -6,7 +6,7 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get install -y op
       zsh stow   
 # Install development packages
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y python-pip build-essential git-core mercurial bzr python-dev ctags cmake software-properties-common python-software-properties 
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y i3 gnome-terminal terminator feh rofi dmenu dunst gnome-settings-daemon xautolock i3blocks xclip 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y i3-wm gnome-terminal terminator feh dmenu dunst gnome-settings-daemon xautolock xclip 
 
 
 
